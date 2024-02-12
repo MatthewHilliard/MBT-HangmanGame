cdd429ec6d07841a1d2c00447d0c27f518d21e05
MatthewHilliard <matthewhilliard2003@gmail.com>
2024-02-12 17:09:01 -0500

    New game button is functional and can pull from word bank in strings xml

--- app/src/main/java/com/example/mbt_hangmangame/MainActivity.kt
+++ app/src/main/java/com/example/mbt_hangmangame/MainActivity.kt
@@ -2,14 +2,37 @@ package com.example.mbt_hangmangame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var hangmanProgress: ImageView
+    private lateinit var guessingWord: TextView
+    private lateinit var newGameButton: Button
+
+    private var currWord: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        hangmanProgress = findViewById(R.id.hangmanPicture)
+        guessingWord = findViewById(R.id.guessingWord)
+        currWord = resources.getStringArray(R.array.wordBank).random()
+        newGameButton = findViewById(R.id.newGameButton)
+
+        newGame()
+
+        newGameButton.setOnClickListener(){
+            newGame()
+        }
     }
-    fun newGame(view: View) {
-        //generate new word?
+    private fun newGame() {
+        hangmanProgress.setImageResource(R.drawable.state0)
+        currWord = resources.getStringArray(R.array.wordBank).random()
+        guessingWord.text = currWord
     }
 }
