6d6704440dc2ff8b73eb40697dbb517de23c5105
picoting <lindsayl.6857@gmail.com>
2024-02-12 17:36:45 -0500

    added num guesses variable bahah

--- app/src/main/java/com/example/mbt_hangmangame/MainActivity.kt
+++ app/src/main/java/com/example/mbt_hangmangame/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var newGameButton: Button
 
     private var currWord: String = ""
+    private var numGuesses: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
